ec8fddac5253d3abd5c8ec5d1d8f82f27df4348a
Gal Miles <58370322+GalMiles@users.noreply.github.com>
2023-06-23 18:09:56 +0300

    changes in clientList

---
+++ client/src/utils/Actions.js
@@ -0,0 +1,7 @@
+const ACTIONS = {
+    JOIN: 'join',
+    LEAVE: 'leave',
+    JOINED: 'joined',
+    DISCONNECTED: 'disconnected',
+    SYNC_CODE: 'sync code'
+};
--- server/server.js
+++ server/server.js
@@ -7,8 +7,13 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 5000;
 
+//every single time a client connect to the server
 io.on('connection', (socket) => {
     console.log('socket connected', socket.id);
+    socket.on('send-code', (code ) => {
+        io.emit('receive-code', code);
+        console.log(code);
+    })
 });
 
 server.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) })

7889c1d4b15c73cca80b31a369a5dc3f5e886fda
Gal Miles <58370322+GalMiles@users.noreply.github.com>
2023-06-27 00:33:48 +0300

    add Highliths.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import LobbyPage from './pages/LobbyPage';
 import {Toaster} from 'react-hot-toast';
-import { useState } from 'react';
 function App() {
 
   return (
--- client/src/components/CodeBlockPage.js
+++ client/src/components/CodeBlockPage.js
@@ -2,11 +2,15 @@
 import { useState, useEffect, useRef } from 'react';
 import Client from './Client';
 import { initSocket } from '../utils/socket';
-
+import hljs from 'highlight.js/lib/core';
+import 'highlight.js/styles/a11y-dark.css';
+import javascript from 'highlight.js/lib/languages/javascript';
+import '../App.css';
 function CodeBlockPage({ blockId }) {
 
   const socketRef = useRef(null);
   const blocks = useRef([]);
+  const textareaRef = useRef(null);
 
   const [code, setCode] = useState('');
   const [isMentor, setIsMentor] = useState(true);
@@ -14,6 +18,7 @@ function CodeBlockPage({ blockId }) {
 
 
   useEffect(() => {
+    
     const visitor = isMentor ? 'Mentor' : 'Student';
     setIsMentor(false);
 
@@ -24,6 +29,10 @@ function CodeBlockPage({ blockId }) {
       socketRef.current.on('code-blocks', (codeBlocks) => {
         blocks.current = codeBlocks;
         const initCode = blocks.current[blockId].code;
+        textareaRef.current.value = initCode;
+        hljs.registerLanguage('javascript', javascript);
+        hljs.highlightBlock(textareaRef.current);
+
         setCode(initCode)
         socketRef.current.emit('codeUpdated', initCode);
        
@@ -54,8 +63,11 @@ function CodeBlockPage({ blockId }) {
   //update sharedCode and send it to the server
   const handleCodeChange = (event) => {
     const newCode = event.target.value;
+    textareaRef.current.value = newCode;
     setCode(newCode);
     socketRef.current.emit('codeUpdated', newCode);
+
+    hljs.highlightBlock(textareaRef.current);
   };
 
   return (
@@ -74,6 +86,7 @@ function CodeBlockPage({ blockId }) {
         </div>
         <div className='editorSide'>
           <textarea
+            ref={textareaRef}
             value={code}
             onChange={handleCodeChange}
             readOnly={isMentor}

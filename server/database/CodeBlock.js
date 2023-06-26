81b2a73705cdb470418c8a6bd0050cd8fdee8211
Gal Miles <58370322+GalMiles@users.noreply.github.com>
2023-06-27 00:54:45 +0300

    restart server

--- server/database/CodeBlock.js
+++ server/database/CodeBlock.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
 
-
 const codeSchema = new mongoose.Schema({
     title: String,
     code: String
 });
 
 mongoose.model('CodeBlock', codeSchema);
-
 module.exports = mongoose.model('CodeBlock');
 
 
